dcb4eaaa3f79e9ba9603486729c0261a61393264
Henry Gordon <HGordon@eagle.org>
2023-03-23 12:56:38 -0500

    Lesson 7 class follow along

---
+++ pages/base_page.py
@@ -0,0 +1,9 @@
+class Page:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, locator):
+        self.driver.find_element(*locator).click()
+
+    def find_element(self,locator):
+        return self.driver.find_element(*locator)
